52c46175b9ab69b1ef97b4c851951945cd5b7de0
Daniele Golzio <80271815+poonchoi@users.noreply.github.com>
2022-05-19 22:54:59 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,41 @@
+import pygame
+import numpy as np
+
+##COLORS##
+red = (255, 0, 0)
+green = (0, 255, 0)
+blue = (0, 0, 255)
+white = (255, 255, 255)
+black = (0, 0, 0)
+##########
+
+##PYGAME INIT##
+pygame.init()
+
+dimensions = 700, 700
+width = dimensions[0]
+height = dimensions[1]
+
+screen_2d = pygame.display.set_mode(dimensions)
+screen_2_5d = pygame.display.set_mode(dimensions)
+
+clock = pygame.time.Clock()
+fps = 60
+
+screen_2d.fill(black)
+###############
+
+
+def draw2d(dimensions):
+    pass
+
+
+run = True
+
+while run:
+    clock.tick(fps)
+    for i in pygame.event.get():
+        if i.type == pygame.QUIT:
+            run = False
+
+    pygame.display.update()
