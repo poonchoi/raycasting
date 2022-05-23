0f7e2d67c817e06b278c8907f73703d1bd8cdeb4
Daniele Golzio <80271815+poonchoi@users.noreply.github.com>
2022-05-23 10:21:51 +0200

    Update main.py

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ black = (0, 0, 0)
 ##PYGAME INIT##
 pygame.init()
 
-dimensions = 700, 700
+dimensions = 1200, 720
 width = dimensions[0]
 height = dimensions[1]
 
@@ -39,7 +39,24 @@ map = [
 ]
 
 def draw_map():
-    pass
+    square_res = 60
+
+    screen_2d.fill(white)
+    # pygame.draw.line(surface, color, start_pos, end_pos, width)
+    # draw vertical lines
+    for i in range(1, square_res + 1):
+        offset = square_res * i
+        start_pos = (0 + offset, 0)
+        end_pos = (0 + offset, height)
+        pygame.draw.line(screen_2d, black, start_pos, end_pos)
+
+    # draw horizontal lines
+    for i in range(1, square_res + 1):
+        offset = square_res * i
+        start_pos = (0, 0 + offset)
+        end_pos = (width, 0 + offset)
+        pygame.draw.line(screen_2d, black, start_pos, end_pos)
+
 
 
 def main():
@@ -52,7 +69,7 @@ def main():
             if i.type == pygame.QUIT:
                 run = False
 
-
+        draw_map()
 
 
         pygame.display.update()
